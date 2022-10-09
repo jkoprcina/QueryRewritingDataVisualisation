f7292b1812b67f9197c8c1958dc0db9c5fe472cc
Josip Koprčina <koprcina.josip@gmail.com>
2022-10-09 17:03:29 +0200

    New version

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
-
 from processing_data import clean_data, split_df
-
 st.set_page_config(layout="wide")
+
 df = pd.read_csv("data/Query-Rewriting.csv")
 df = clean_data(df)
-df_personal_canonical, df_personal_not_canonical, df_base_canonical, df_base_not_canonical = split_df(df)
-df_dictionary = {'base_canonical': df_base_canonical, 'base_non_canonical': df_base_not_canonical,
-                 'personal_canonical': df_personal_canonical, 'personal_non_canonical': df_personal_not_canonical}
+st.write(df)
+df_manual, df_testing_retriver_reranker, df_personal_testing_query_rewriting, df_personal_testing_everything, df_base_testing_query_rewriting, df_base_testing_everything = split_df(df)
+df_dictionary = {
+    'base_testing_query_rewriting': df_base_testing_query_rewriting,
+    'base_testing_everything': df_base_testing_everything,
+    'personal_testing_query_rewriting': df_personal_testing_query_rewriting,
+    'personal_testing_everything': df_personal_testing_everything
+}
 
+st.write(df_personal_testing_query_rewriting)
+st.write(df_personal_testing_everything)
 
 col1, col2, col3 = st.columns(3)
 with col1:
     T5_config = st.radio("What T5 config would you like to see?", ('base', 'personal'))
 with col2:
-    canonical = st.radio("Would you like to see canonical or non-canonical?", ('canonical', 'non_canonical'))
+    canonical = st.radio("Would you like to see canonical or non-canonical?",
+                         ('testing_query_rewriting', 'testing_everything'))
 with col3:
-    score = st.radio("Would you like to see the bleu or rouge scores?", ('bleu', 'rouge'))
+    score = st.radio("Would you like to see the bleu or rouge scores?", ('bleu', 'rouge', 'ndcg'))
 
 st.subheader("You're looking at a graph showing the " + score + " score, ran on the T5 " + T5_config +
              " configuration ran " + canonical)
@@ -26,7 +33,10 @@ st.subheader("You're looking at a graph showing the " + score + " score, ran on 
 x = [i for i in range(2, 11)]
 fig = go.Figure()
 for index, row in df_dictionary.get(T5_config + "_" + canonical).iterrows():
-    fig.add_trace(go.Scatter(x=x, y=row[score + "_scores"], mode='lines', name=row['sent_extra']))
+    fig.add_trace(go.Scatter(x=x, y=row[score + "_scores"], mode='lines', name=row['data_usage_type']))
+if score == "ndcg":
+    for index, row in df_testing_retriver_reranker.iterrows():
+        fig.add_trace(go.Scatter(x=x, y=row[score + "_scores"], mode='lines', name="golden standard"))
 fig.update_layout(width=1400, height=700)
 st.plotly_chart(fig, use_container_width=False, sharing="streamlit")
 
--- processing_data.py
+++ processing_data.py
@@ -1,17 +1,22 @@
 def clean_data(df):
     df = df.drop(['Id', 'Creation Time'], axis=1)
-    bleu, rouge = [], []
+    bleu, rouge, ndcg = [], [], []
     for index, row in df.iterrows():
         bleu.append([row["b_s_2"], row["b_s_3"], row["b_s_4"], row["b_s_5"], row["b_s_6"], row["b_s_7"], row["b_s_8"],
                      row["b_s_9"], row["b_s_10"]])
         rouge.append([row["r_s_2 f_measure"], row["r_s_3 f_measure"], row["r_s_4 f_measure"], row["r_s_5 f_measure"],
                       row["r_s_6 f_measure"], row["r_s_7 f_measure"], row["r_s_8 f_measure"], row["r_s_9 f_measure"],
                       row["r_s_10 f_measure"]])
+        ndcg.append([row["ndcg_2"], row["ndcg_3"], row["ndcg_4"], row["ndcg_5"], row["ndcg_6"], row["ndcg_7"],
+                     row["ndcg_8"], row["ndcg_9"], row["ndcg_10"]])
+
     df = df.drop(['b_s_2', 'b_s_3', 'b_s_4', 'b_s_5', 'b_s_6', 'b_s_7', 'b_s_8', 'b_s_9', 'b_s_10', 'r_s_2 f_measure',
                   'r_s_3 f_measure', 'r_s_4 f_measure', 'r_s_5 f_measure', 'r_s_6 f_measure', 'r_s_7 f_measure',
-                  'r_s_8 f_measure', 'r_s_9 f_measure', 'r_s_10 f_measure'], axis=1)
+                  'r_s_8 f_measure', 'r_s_9 f_measure', 'r_s_10 f_measure', 'ndcg_2', 'ndcg_3', 'ndcg_4', 'ndcg_5',
+                  'ndcg_6', 'ndcg_7', 'ndcg_8', 'ndcg_9', 'ndcg_10'], axis=1)
     df = df.assign(bleu_scores=bleu)
     df = df.assign(rouge_scores=rouge)
+    df = df.assign(ndcg_scores=ndcg)
     return df
 
 
@@ -26,16 +31,21 @@ def extract_data_for_line(df):
 
 def split_df(df):
     df_personal = df[df['T5_query_rewriter'] == "personal"]
-    df_personal_canonical = df_personal[df_personal['canonical']]
-    df_personal_not_canonical = df_personal[df_personal['canonical'] != True]
+    df_personal_testing_query_rewriting = df_personal[df_personal['run_type'] == "testing_query_rewriting"]
+    df_personal_testing_everything = df_personal[df_personal['run_type'] == "testing_everything"]
+
     df_base = df[df['T5_query_rewriter'] == "base"]
-    df_base_canonical = df_base[df_base['canonical']]
-    df_base_not_canonical = df_base[df_base['canonical'] != True]
+    df_base_testing_query_rewriting = df_base[df_base['run_type'] == "testing_query_rewriting"]
+    df_base_testing_everything = df_base[df_base['run_type'] == "testing_everything"]
 
-    df_personal_canonical = df_personal_canonical.drop(['canonical', 'T5_query_rewriter'], axis=1)
-    df_personal_not_canonical = df_personal_not_canonical.drop(['canonical', 'T5_query_rewriter'], axis=1)
-    df_base_canonical = df_base_canonical.drop(['canonical', 'T5_query_rewriter'], axis=1)
-    df_base_not_canonical = df_base_not_canonical.drop(['canonical', 'T5_query_rewriter'], axis=1)
+    df_manual = df[df['run_type'] == "manual"]
+    df_testing_retriever_reranker = df[df["run_type"] == "testing_retriever_reranker"]
 
-    return df_personal_canonical, df_personal_not_canonical, df_base_canonical, df_base_not_canonical
+    df_personal_testing_query_rewriting = df_personal_testing_query_rewriting.drop(['run_type', 'T5_query_rewriter'], axis=1)
+    df_personal_testing_everything = df_personal_testing_everything.drop(['run_type', 'T5_query_rewriter'], axis=1)
+    df_base_testing_query_rewriting = df_base_testing_query_rewriting.drop(['run_type', 'T5_query_rewriter'], axis=1)
+    df_base_testing_everything = df_base_testing_everything.drop(['run_type', 'T5_query_rewriter'], axis=1)
+
+    return (df_manual, df_testing_retriever_reranker, df_personal_testing_query_rewriting,
+            df_personal_testing_everything, df_base_testing_query_rewriting, df_base_testing_everything)
 
